0666ea6cf92f274c6d992ec832891847ca36c9c0
Vital Ozierski <v.ozierski@ozitag.com>
2020-08-14 11:31:02 +0300

    fix 404 for templates requests

--- src/Controllers/AdminTemplatesController.php
+++ src/Controllers/AdminTemplatesController.php
@@ -4,6 +4,8 @@ namespace OZiTAG\Tager\Backend\Pages\Controllers;
 
 use OZiTAG\Tager\Backend\Core\Controllers\Controller;
 use OZiTAG\Tager\Backend\Crud\Controllers\CrudController;
+use OZiTAG\Tager\Backend\Pages\Features\Admin\TemplatesFeature;
+use OZiTAG\Tager\Backend\Pages\Features\Admin\ViewTemplateFeature;
 
 class AdminTemplatesController extends Controller
 {
