f950f64aa0ce5665ad75a936e4a259c97bd3b7ea
Vital Ozierski <v.ozierski@ozitag.com>
2020-10-29 02:05:53 +0300

    RBAC middleware

--- routes/routes.php
+++ routes/routes.php
@@ -5,6 +5,8 @@ use OZiTAG\Tager\Backend\Pages\Controllers\PublicController;
 use OZiTAG\Tager\Backend\Pages\Controllers\AdminController;
 use OZiTAG\Tager\Backend\Pages\Controllers\AdminTemplatesController;
 use OZiTAG\Tager\Backend\Pages\Controllers\AdminPagesController;
+use OZiTAG\Tager\Backend\Rbac\Facades\AccessControlMiddleware;
+use OZiTAG\Tager\Backend\Pages\Enums\PageScope;
 
 Route::group(['middleware' => 'api.cache'], function () {
     Route::get('/tager/pages', [PublicController::class, 'index']);
@@ -13,16 +15,30 @@ Route::group(['middleware' => 'api.cache'], function () {
 });
 
 Route::group(['prefix' => 'admin/pages', 'middleware' => ['passport:administrators', 'auth:api']], function () {
-    Route::get('/info', [AdminController::class, 'moduleInfo']);
 
-    Route::get('/templates', [AdminTemplatesController::class, 'index']);
-    Route::get('/templates/{alias}', [AdminTemplatesController::class, 'view']);
+    Route::group(['middleware' => [AccessControlMiddleware::scopes(PageScope::View)]], function () {
+        Route::get('/info', [AdminController::class, 'moduleInfo']);
+        Route::get('/count', [AdminPagesController::class, 'count']);
+        Route::get('/', [AdminPagesController::class, 'index']);
 
-    Route::get('/count', [AdminPagesController::class, 'count']);
-    Route::get('/', [AdminPagesController::class, 'index']);
-    Route::post('/', [AdminPagesController::class, 'store']);
-    Route::put('/{id}', [AdminPagesController::class, 'update']);
-    Route::get('/{id}', [AdminPagesController::class, 'view']);
-    Route::delete('/{id}', [AdminPagesController::class, 'delete']);
-    Route::post('/{id}/move/{direction}', [AdminPagesController::class, 'move'])->where('direction', 'up|down');
+
+        Route::group(['middleware' => [AccessControlMiddleware::scopes(PageScope::View)]], function () {
+            Route::get('/templates', [AdminTemplatesController::class, 'index']);
+            Route::get('/templates/{alias}', [AdminTemplatesController::class, 'view']);
+            Route::get('/{id}', [AdminPagesController::class, 'view']);
+        });
+
+        Route::group(['middleware' => [AccessControlMiddleware::scopes(PageScope::Create)]], function () {
+            Route::post('/', [AdminPagesController::class, 'store']);
+        });
+
+        Route::group(['middleware' => [AccessControlMiddleware::scopes(PageScope::Edit)]], function () {
+            Route::put('/{id}', [AdminPagesController::class, 'update']);
+            Route::post('/{id}/move/{direction}', [AdminPagesController::class, 'move'])->where('direction', 'up|down');
+        });
+
+        Route::group(['middleware' => [AccessControlMiddleware::scopes(PageScope::Delete)]], function () {
+            Route::delete('/{id}', [AdminPagesController::class, 'delete']);
+        });
+    });
 });
