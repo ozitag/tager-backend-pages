96232d1d2333c75067c75a64595d18826f633a9e
Vital Ozierski <v.ozierski@ozitag.com>
2021-04-16 14:48:49 +0300

    fix typos

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -4,5 +4,5 @@ return [
     'template_not_found' => 'Template not found',
     'page_not_found' => 'Page not found',
     'create_page_failed' => 'Error create page',
-    'url_busy' => 'Страница с URL ":url_path" уже существует'
+    'url_busy' => 'The page with URL ":url_path" already exists'
 ];
--- resources/lang/ru/errors.php
+++ resources/lang/ru/errors.php
@@ -4,5 +4,5 @@ return [
     'template_not_found' => 'Шаблон не найден',
     'page_not_found' => 'Страница не найдена',
     'create_page_failed' => 'Ошибка создания страницы',
-    'url_busy' => 'The page with URL ":url_path" already exists'
+    'url_busy' => 'Страница с URL ":url_path" уже существует'
 ];
