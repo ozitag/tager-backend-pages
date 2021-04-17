d79cf003027ea4d808c8fb5e8b217d9c7abbdf2e
Vital Ozierski <v.ozierski@ozitag.com>
2021-04-17 23:23:30 +0300

    improve sitemap handler

--- src/PagesSitemapHandler.php
+++ src/PagesSitemapHandler.php
@@ -3,14 +3,14 @@
 namespace OZiTAG\Tager\Backend\Pages;
 
 use Carbon\Carbon;
+use OZiTAG\Tager\Backend\Pages\Models\TagerPage;
 use OZiTAG\Tager\Backend\Pages\Repositories\PagesRepository;
 use OZiTAG\Tager\Backend\Seo\Contracts\ISitemapHandler;
 use OZiTAG\Tager\Backend\Seo\Structures\SitemapItem;
 
 class PagesSitemapHandler implements ISitemapHandler
 {
-    /** @var PagesRepository */
-    private $pagesRepository;
+    private PagesRepository $pagesRepository;
 
     public function __construct(PagesRepository $pagesRepository)
     {
@@ -19,11 +19,12 @@ class PagesSitemapHandler implements ISitemapHandler
 
     public function handle()
     {
+        /** @var TagerPage[] $pages */
         $pages = $this->pagesRepository->all();
 
         $result = [];
         foreach ($pages as $page) {
-            $result[] = new SitemapItem($page->url_path, new Carbon($page->updated_at));
+            $result[] = new SitemapItem($page->getWebPageUrl(), new Carbon($page->updated_at));
         }
 
         return $result;
